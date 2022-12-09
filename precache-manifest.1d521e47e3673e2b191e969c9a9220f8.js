dddab267c33e376cba287c04b9ec21ed695d9255
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-08 23:07:39 -0700

    Updates

---
+++ precache-manifest.1d521e47e3673e2b191e969c9a9220f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3b6a48a38ef270cae618091a0a6a9c",
+    "url": "/review-react/index.html"
+  },
+  {
+    "revision": "b3b176803489310a02cf",
+    "url": "/review-react/static/css/main.972001a6.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/review-react/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/review-react/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b176803489310a02cf",
+    "url": "/review-react/static/js/main.1be4ad2d.chunk.js"
+  },
+  {
+    "revision": "59149ced1f92b7c9ebbe",
+    "url": "/review-react/static/js/runtime-main.fdc15e76.js"
+  }
+]);
